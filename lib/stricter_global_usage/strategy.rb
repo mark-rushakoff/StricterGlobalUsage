4f0de10a201401b1e9e451e6dc2ebe9a17392370
Mark Rushakoff <mark.rushakoff@gmail.com>
2012-06-13 07:41:42 -0700

    Documentation for Strategy

--- lib/stricter_global_usage/strategy.rb
+++ lib/stricter_global_usage/strategy.rb
@@ -1,19 +1,46 @@
 module StricterGlobalUsage
+  # The Strategy module represents the action to take when a function is called and its argument defaults to the value
+  # of a global variable.
+  #
+  # Consumers of StricterGlobalUsage usually will only need to use the +use+ method to specify which strategy should be
+  # applied when necessary.
+  #
+  # There are three built-in strategies (although consumers are free to add more through the +add+ method):
+  #
+  # * +raise+: The default strategy. Raises ::StricterGlobalUsage::DefaultGlobalVariableUsed exception.
+  # * +warn+: Logs the call with Kernel.warn, then continues to execute the original method.
+  # * +silent+: Does nothing, then continues to execute the original method.
   module Strategy
     @strategies = {}
     @name = nil
+
+    # Apply the current strategy (warns, raises an exception, or does something else the user chose).
     def self.apply
       @strategies[@name].call
     end
 
+    # Create a new strategy.
+    #
+    #   ::StricterGlobalUsage::Strategy.add(:my_log) do
+    #     MyLogger.log('Used a function whose argument fell back to a global variable')
+    #   end
     def self.add(name, &block)
       @strategies[name] = block
     end
 
+    # Use the strategy represented by the given name.
+    #
+    #   ::StricterGlobalUsage::Strategy.use(:my_log)
     def self.use(name)
       @name = name
     end
 
+    # Use the strategy represented by the given name for the context of the given block,
+    # then restore the previous strategy.
+    #
+    #   ::StricterGlobalUsage::Strategy.with(:silent) do
+    #     ::SomeVendorLibrary.do_stuff
+    #   end
     def self.with(name, &block)
       orig_name = @name
       begin
@@ -29,7 +56,7 @@ module StricterGlobalUsage
     end
 
     add(:warn) do
-      ::Kernel.warn('')
+      ::Kernel.warn('Called a method that used a default argument of a global variable')
     end
 
     add(:silent) { }
--- lib/stricter_global_usage/version.rb
+++ lib/stricter_global_usage/version.rb
@@ -1,4 +1,4 @@
 module StricterGlobalUsage
   # stricter_global_usage version
-  VERSION = "0.1.0"
+  VERSION = "1.0"
 end
